2ca21778f38667544c7990a920d1c396fcd3f96b
Kadoia614 <miguel_moraes2001@hotmail.com>
2025-06-13 16:38:18 -0300

    second part of att

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
-import { UserContext } from "./UserContextFile";
+import { useState, createContext } from "react";
+
 
 export const UserProvider = ({ children }) => {
   let [scopo, setScopo] = useState(null);
   let [auth, setAuth] = useState(false);
   let [userServices, setUserServices] = useState([]);
+const UserContext = createContext();
 
   return (
     <UserContext.Provider value={{ scopo, setScopo, auth, setAuth, userServices, setUserServices }}>
