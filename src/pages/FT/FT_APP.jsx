fe4f10fc18dfadfeb04a2e4d0a6f6fdbb6904b52
Kadoia614 <miguel_moraes2001@hotmail.com>
2025-06-17 10:15:11 -0300

    fix: Update title in FT_APP component for clarity

--- src/pages/FT/FT_APP.jsx
+++ src/pages/FT/FT_APP.jsx
@@ -71,7 +71,7 @@ const FTAPP = () => {
 
   return (
     <div id="Bolsistas" className="content">
-      <Title>Com grandes poderes vêm grandes responsabilidades</Title>
+      <Title>Frente de Trabalho</Title>
 
       <div>
         <BolsistasTable
