0e9eef7c5a344350b773c325df430cc13934d362
Kadoia614 <miguel_moraes2001@hotmail.com>
2025-10-09 16:35:47 -0300

    Add: Adicionado variaveis de ambiente e aplicado na tela de APP

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
+      "@context": path.resolve(__dirname, "src/context"),
       "@api": path.resolve(__dirname, "src/api"),
     },
   },
