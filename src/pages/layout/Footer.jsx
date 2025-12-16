f651105e68e0436c356ec2f44f2c1215f8b9a785
Kadoia614 <miguel_moraes2001@hotmail.com>
2025-12-16 10:01:35 -0300

    Update footer text to include 'Equipe de desenvolvimento'

--- src/pages/layout/Footer.jsx
+++ src/pages/layout/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="footer w-full z-1">
       <div className="footer_container bg-primary text-on-primary p-4 text-center">
-        <h2 className="text-sm">Itapecerica da Serra © Todos os direitos reservados: 2025</h2>
+        <h2 className="text-sm"> Equipe de desenvolvimento de Itapecerica da Serra © Todos os direitos reservados: 2025</h2>
       </div>
     </div>
   );
