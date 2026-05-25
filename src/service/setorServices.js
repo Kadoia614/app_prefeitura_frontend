5faf835b7fa014e6e136cc65ef88330358884de3
Kadoia614 <miguel_moraes2001@hotmail.com>
2026-05-25 16:25:28 -0300

    Fix: Correct API endpoints for saving and updating 'setor' to use plural form

--- src/service/setorServices.js
+++ src/service/setorServices.js
@@ -1,11 +1,11 @@
 import API from "@api/API";
 
 export const saveSetor = async (setor) => {
-  const { data } = await API.post("/setor", setor);
+  const { data } = await API.post("/setores", setor);
   return data;
 };
 
 export const updateSetor = async (setor, id) => {
-  const { data } = await API.put(`/setor/${id}`, setor);
+  const { data } = await API.put(`/setores/${id}`, setor);
   return data;
 };
